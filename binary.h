3abd84a6ef0aca082fe9e09b45b6805f6f0f94fc
zhongjn <2389206378@qq.com>
2019-12-30 22:57:31 +0800

    refactor

--- binary.h
+++ binary.h
@@ -7,25 +7,12 @@
 class BinaryWriter;
 class BinaryReader;
 
-//class ISerializable {
-//public:
-//    virtual void serialize(BinaryWriter& writer) {}
-//    virtual void deserialize(BinaryReader& reader) {}
-//};
-
 template<typename T>
 struct Serializer {
     static_assert("No Serializer<T> provided");
     void serialize(BinaryWriter& bw, const T& t) {}
     void deserialize(BinaryReader& br, T& t) {}
 };
-
-//template<typename T>
-//struct EmptySerializer {
-//    void serialize(BinaryWriter& bw, const T& t) {}
-//    void deserialize(BinaryReader& br, T& t) {}
-//};
-
 
 class BinaryWriter
 {
@@ -140,3 +127,24 @@ struct Serializer<std::vector<T>> {
         }
     }
 };
+
+
+// this looks evil
+// maybe should not be used
+#define SERIALIZER(type, ser, deser) template<> struct Serializer<type> { \
+    void serialize(BinaryWriter& bw, const type& obj) { do ser while(0); } \
+    void deserialize(BinaryReader& br, const type& obj) { do deser while (0); } \
+};
+
+#define FIELD0(p, fld)
+#define FIELD1(p, fld) p(obj.##fld);
+#define FIELD2(p, fld, ...) _CONCAT(FIELD1(p, fld), FIELD1(p, __VA_ARGS__))
+#define FIELD3(p, fld, ...) _CONCAT(FIELD1(p, fld), FIELD2(p, __VA_ARGS__))
+#define FIELD4(p, fld, ...) _CONCAT(FIELD1(p, fld), FIELD3(p, __VA_ARGS__))
+
+#define _SERIALIZERN(n, type, ...) SERIALIZER(type, { FIELD##n(bw.write, __VA_ARGS__) }, { FIELD##n(br.read, __VA_ARGS__) })
+#define SERIALIZER0(type, ...) _SERIALIZERN(0, type, __VA_ARGS__)
+#define SERIALIZER1(type, ...) _SERIALIZERN(1, type, __VA_ARGS__)
+#define SERIALIZER2(type, ...) _SERIALIZERN(2, type, __VA_ARGS__)
+#define SERIALIZER3(type, ...) _SERIALIZERN(2, type, __VA_ARGS__)
+#define SERIALIZER4(type, ...) _SERIALIZERN(2, type, __VA_ARGS__)
